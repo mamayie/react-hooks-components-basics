59ff4ab69b9d1aed6c57776391d39e66a3f0f764
mamayie <mappiah319@gmail.com>
2022-07-20 11:06:43 +0000

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
-function App() {
+function Article() {
   return (
+    <div>
+      <div> Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    </div>
+  );
+}
+
+function Comment() {
+  return (
+    <div>
+      <div>Naturally, I agree with this article.</div>
+    </div>
+  );
+}
+
+function App(){
+  return(
     <div>
       <Article />
       <Comment />
     </div>
-  );
+  )
 }
 
 export default App;
